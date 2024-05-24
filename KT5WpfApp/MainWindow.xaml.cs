9cfc4eb8e5ec1b6c7f2cef92f8f98e87a2257394
Maksim9056 <bobreczovm@inbox.ru>
2024-05-24 16:33:21 +0300

    Добавьте файлы проекта.

---
+++ KT5WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Kt5WCF;
+using LiveCharts.Wpf;
+using LiveCharts;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using LiveCharts.Wpf.Charts.Base;
+namespace KT5WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<OrderStatus> OrderStatuses { get; set; }
+        public SeriesCollection PieSeriesCollection { get; set; }
+        public ObservableCollection<LineSeries> LineSeriesCollection { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            OrderStatuses = new ObservableCollection<OrderStatus>();
+            PieSeriesCollection = new SeriesCollection(); // Инициализируем PieSeriesCollection
+            LineSeriesCollection = new ObservableCollection<LineSeries>();
+            DataContext = this;
+
+            LoadOrderStatuses();
+        }
+
+        private void LoadOrderStatuses()
+        {
+          
+
+            var client = new OrderService();
+            var statuses = client.GetOrderStatuses();
+            OrderStatuses.Clear();
+            foreach (var status in statuses)
+            {
+                OrderStatuses.Add(status);
+            }
+
+            // Очищаем и затем обновляем данные для круговой диаграммы
+            PieSeriesCollection.Clear();
+            foreach (var status in OrderStatuses)
+            {
+                PieSeriesCollection.Add(new PieSeries
+                {
+                    Title = status.Status,
+                    Values = new ChartValues<int> { status.Count },
+                    DataLabels = true
+                });
+                LineSeriesCollection.Add(new LineSeries
+                {
+                    Title = status.Status,
+                    Values = new ChartValues<int> { status.Count },
+                   
+                }); 
+                ColumnSeries mySeries = new ColumnSeries
+                {
+                    Title = status.Status,
+                    Values = new ChartValues<int> { status.Count },
+                    DataLabels = true,
+                  
+                };
+
+                myChart.Series.Add(mySeries);
+            }
+            
+        }
+    }
+}
---
+++ Kt5WCF/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Kt5WCF
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+
+    public class OrderService : IOrderService
+    {
+        public List<OrderStatus> GetOrderStatuses()
+        {
+            // Пример данных
+            return new List<OrderStatus>
+        {
+            new OrderStatus { Status = "Processing", Count = 5 },
+            new OrderStatus { Status = "Shipped", Count = 10 },
+            new OrderStatus { Status = "Delivered", Count = 15 }
+        };
+        }
+    }
+
+}
